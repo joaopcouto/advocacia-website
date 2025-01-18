6f22802be27dc453476473483cf6a184d352ab5f
João Pedro Couto <129805937+joaopcouto@users.noreply.github.com>
2025-01-18 14:10:35 -0300

    Reworked navbar and footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,25 +1,57 @@
-import React from 'react'
-import './styles.css'
-import logoImg from '../../assets/images/logo.png'
-import locationImg from '../../assets/icons/navigation-arrow-bold.svg'
-import emailImg from '../../assets/icons/envelope-simple-bold.svg'
-import phoneImg from '../../assets/icons/phone-bold.svg'
-import whatsappImg from '../../assets/icons/whatsapp-logo-bold.svg'
-import instagramImg from '../../assets/icons/instagram-logo-bold.svg'
+import React from "react";
+import "./styles.css";
+import logoImg from "../../assets/images/logo.png";
+import arrowbrown from "../../assets/icons/arrow-brown.svg";
+import phonebrown from "../../assets/icons/phone-brown.svg";
+import phonebrown2 from "../../assets/icons/phone-brown2.svg";
+import envelope from "../../assets/icons/envelope-brown.svg";
+
+import locationImg from "../../assets/icons/navigation-arrow-bold.svg";
+import emailImg from "../../assets/icons/envelope-simple-bold.svg";
+import phoneImg from "../../assets/icons/phone-bold.svg";
+import whatsappImg from "../../assets/icons/whatsapp-logo-bold.svg";
+import instagramImg from "../../assets/icons/instagram-logo-bold.svg";
 
 export const Footer: React.FC = () => {
   return (
     <footer>
-      <div className='footer-content'>
-        <div  className='footer-brand'>
-       <img className='logoImg' src={logoImg} alt="logo" />
-       <p className='text-oab'>OAB/SP 230.817 </p>
+      <div className="footer-brand">
+        <img className="logoImg" src={logoImg} alt="logo" />
+        <p className="text-oab">OAB/SP 230.817 </p>
+      </div>
+
+      <div className="footer-location">
+        <div className="header-location">
+          <h2 className="title-location">Localização</h2>
+          <img className="arrow-location" src={arrowbrown} alt="" />
         </div>
-       
 
-      <div className='footer-contact'>
+        <div className="description-location">
+          <p className="description-location-text">
+            Rua Capitão João José de Macedo, nº 340, 3° andar
+            </p>
+          <p>
+            Jacareí - São Paulo | CEP 12327-030
+          </p>
+        </div>
+      </div>
 
-      <ul className='footer-list'>
+      <div className="footer-contact">
+        <div className="header-contact">
+          <h2 className="title-contact">Fale conosco</h2>
+          <img src={phonebrown} alt="" />
+        </div>
+
+        <div className="description-contact-email">
+          <img src={envelope} alt="" />
+          <p className="text-email">advocaciafreitascouto@gmail.com</p>
+        </div>
+        <div className="description-contact-phone">
+          <img className="text-phone" src={phonebrown2} alt="" />
+          <a className="text-phone" href="tel:5512997398999">55 (12) 99739-8999</a>
+        </div>
+
+        {/* <ul className='footer-list'>
         
         <li>
         <img src={locationImg} alt="" />
@@ -41,12 +73,8 @@ export const Footer: React.FC = () => {
         <img src={instagramImg} alt="" />
           <a target='_blank' href="https://www.instagram.com/advocaciafreitascouto/">@advocaciafreitascouto</a>
         </li>
-      </ul>
-
-      </div>
-
-
+      </ul> */}
       </div>
     </footer>
-  )
-}
+  );
+};
--- frontend/src/components/Navbar/Header.tsx
+++ frontend/src/components/Navbar/Header.tsx
@@ -1,30 +1,59 @@
-import { React, useState } from 'react';
+import { React, useEffect, useState } from 'react';
 import logoImg from '../../assets/images/logo.png';
 import  './styles.css';
 
-import { Link } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 
 
 export const Header: React.FC = () => {
+    
+    const location = useLocation();
+
+    useEffect(() => {
+        const pathname = location.pathname;
+        const encodedPathname = encodeURI(pathname);
+        const selector = `a[href="${encodedPathname}"]`;
+  
+        // Encontre o link correspondente e aplique o estilo
+        const link = document.querySelector(selector);
+        if (link) {
+          link.style.color = '#B4814A';
+        }
+  
+  
+        // Limpe o estilo do link anterior quando a rota mudar
+        return () => {
+            const previousLinks = document.querySelectorAll('nav a');
+            previousLinks.forEach((link) => {
+               link.style.color = '#B1997B'; // Cor padrão do link
+            });
+  
+        }
+    }, [location.pathname]);
+  
+
   return (
     <header>
         <nav id='navbar'>
 
             <div className='nav-brand'>
                 <Link to={'/'}>
-                <img className='logoImg' src={logoImg} alt="logo" />             
+                <img className='logoImg' id='nav' src={logoImg} alt="logo" />             
                 </Link>
             </div>
 
            <ul className='nav-list'>
             <li>
-                <Link to='/'>Home</Link>
+                <Link to='/Home'>Home</Link>
             </li>
             <li>
-                <Link to='/institucional'>Institucional</Link>
+                <Link to='/sobre'>Sobre</Link>
             </li>
             <li>
                 <Link to='/especialidades'>Especialidades</Link>
+            </li>
+            <li>
+                <Link to='/Noticias'>Notícias</Link>
             </li>
             <li>
                 <Link to='/contato'>Contato</Link>
